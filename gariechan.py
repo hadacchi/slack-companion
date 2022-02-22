6a3be50e6282de34852ca7f67ab96f1cc9c4ef0d
hadacchi <hadacchi@users.noreply.github.com>
2022-02-22 19:22:53 +0900

    get video ids from slack replies, apply pep8

--- gariechan.py
+++ gariechan.py
@@ -13,8 +13,8 @@ import msgs
 # set log level at main function
 # if program will be completed, change to logging.ERROR
 # when program should be debugged, change to logging.DEBUG
-#logging.basicConfig(level=logging.INFO)
-#logging.basicConfig(level=logging.DEBUG)
+# logging.basicConfig(level=logging.INFO)
+# logging.basicConfig(level=logging.DEBUG)
 
 config = toml.load(open('secret.toml'))
 
@@ -34,6 +34,45 @@ del client
 app = App(token=slack_bot_token)
 
 # イベント API
+
+
+@app.message(re.compile(f'<@{bot_user_id}> *pick$'))
+def pick_message(message, say, logger, context):
+    '''pick message from the ch
+    '''
+
+    ch = message['channel']
+    ts = message['ts']
+    #client = WebClient(token=slack_bot_token)
+    client = WebClient(token=slack_user_token)
+
+    logger.error(str(message))
+    #say(msgs.confirm())
+
+
+    count = 5
+
+    while True:
+        if count <= 0:
+            break
+        limit = min(count, 100)
+        respond, msg_list, has_more = msgp.get_history(
+            ch, client, say, logger, latest=ts, limit=limit)
+        if respond is None:
+            break
+
+        #logger.debug(str(msg_list))
+        replies = msgp.get_replies(message['channel'], msg_list, client, logger)
+        #logger.debug(str(replies))
+        count -= len(msg_list)
+
+        if has_more:
+            continue
+        break
+
+    msgp.rm_history(ch, ts, client, logger)
+
+
 @app.message(re.compile(f'<@{bot_user_id}> *clear *([^ ]*)$'))
 def handle_message_events(message, say, logger, context):
     '''clear all histories
@@ -58,22 +97,14 @@ def handle_message_events(message, say, logger, context):
         if count <= 0:
             break
 
-        limit = min(count,100)
-        respond, msg_list, has_more = msgp.get_history( message['channel']
-                                                    , client
-                                                    , say
-                                                    , logger
-                                                    , latest=ts
-                                                    , limit=limit
-                                                    )
+        limit = min(count, 100)
+        respond, msg_list, has_more = msgp.get_history(
+            message['channel'], client, say, logger, latest=ts, limit=limit)
         if respond is None:
             break
 
-        respond = msgp.clear_history( message['channel']
-                                    , msg_list
-                                    , client
-                                    , logger
-                                    )
+        respond = msgp.clear_history(
+            message['channel'], msg_list, client, logger)
         count -= len(msg_list)
 
         if has_more:
@@ -83,6 +114,7 @@ def handle_message_events(message, say, logger, context):
     if respond is not None:
         # last respond is output
         say(respond)
+
 
 @app.message(re.compile(f'<@{bot_user_id}> *([W|w]here)'))
 def return_joining_channels(say, logger, context):
@@ -99,25 +131,27 @@ def return_joining_channels(say, logger, context):
         members = client.conversations_members(channel=ch['id'])['members']
         if bot_user_id in members:
             channels.append(ch['name'])
-    if len(channels)>0:
+    if len(channels) > 0:
         say(msgs.imin().format('\n'.join(channels)))
     else:
         say(msgs.noplace())
+
 
 @app.event('message')
 def nop(message, logger):
     logger.debug(str(message))
 
+
 if __name__ == "__main__":
     # if program will be completed, change to logging.ERROR
     # when program should be debugged, change to logging.DEBUG
     logger = logging.getLogger()
-    logger.setLevel(logging.INFO)
-    #logger.setLevel(logging.DEBUG)
+    #logger.setLevel(logging.INFO)
+    logger.setLevel(logging.DEBUG)
 
     # log file
-    fh = logging.FileHandler('garie.log')
-    logger.addHandler(fh)
+    #fh = logging.FileHandler('garie.log')
+    #logger.addHandler(fh)
 
     handler = SocketModeHandler(app, slack_app_token)
     handler.start()
--- msgprocessing.py
+++ msgprocessing.py
@@ -2,6 +2,43 @@ import json
 import time
 
 import msgs
+import pickle
+
+
+def get_replies(ch, msg_list, client, logger=None):
+    if logger is not None:
+        logger.error('Now get replies')
+
+    video_ids = []
+    for i, msg in enumerate(msg_list):
+        if 'reply_count' in msg:
+            logger.error(f'reply_count is {msg["reply_count"]}')
+            res = client.conversations_replies(channel=ch, ts=msg['ts'])
+            if not res['ok']:
+                if logger is not None:
+                    logger.error(str(res))
+                else:
+                    print(res)
+                say(msgs.fail())
+                return None, [], False
+            else:
+                data = res['messages']
+                listname = data[0]['text']
+                for m in data[1:]:
+                    if m['text'].find('www.youtube.com') > 0:
+                        # youtube
+                        video_ids.append(m['text'].replace(
+                            '>', '').split('=')[-1])
+                    elif m['text'].find('youtu.be') > 0:
+                        # youtu.be
+                        video_ids.append(m['text'].replace(
+                            '>', '').split('/')[-1])
+                    else:
+                        logger.error(f"unknown url {m['text']}")
+            pickle.dump(video_ids, open('res.pkl', 'wb'))
+            return
+            # logger.error(str(res))
+
 
 def clear_history(ch, msg_list, client, logger=None):
     if logger is not None:
@@ -13,15 +50,16 @@ def clear_history(ch, msg_list, client, logger=None):
     logger.info(f'{i+1} messages were deleted')
     return msgs.finish()
 
+
 def get_history(ch, client, say, logger=None, limit=100, latest='now'):
     if logger is not None:
         logger.debug('Now get history')
 
-    res = client.conversations_history( channel=ch
-                                      , limit=limit
-                                      , inclusive=True
-                                      , latest=latest
-                                      )
+    res = client.conversations_history(
+        channel=ch,
+        limit=limit,
+        inclusive=True,
+        latest=latest)
     if not res['ok']:
         if logger is not None:
             logger.error(str(res))
@@ -33,3 +71,11 @@ def get_history(ch, client, say, logger=None, limit=100, latest='now'):
         if logger is not None:
             logger.debug(f"get {len(res['messages'])} messages")
         return msgs.confirm(), res['messages'], res['has_more']
+
+
+def rm_history(ch, ts, client, logger=None):
+    if logger is not None:
+        logger.debug('Now rm history')
+
+    res = client.chat_delete(channel=ch, ts=ts)
+    return msgs.finish()
